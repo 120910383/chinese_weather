fd03e363763db44b80ef2a1ef965ee882f489954
zhanghao <120910383@qq.com>
2014-03-29 15:47:21 +0800

    天气json字符串解析测试，由于之前的lib_json.lib编译为MTd导致错误，这里用MDd方式重新链接生成lib_json.lib使用即不报错

--- chinese_weather/win32/test.cpp
+++ chinese_weather/win32/test.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #include "../classes/define.h"
 #include "../classes/httpclient.h"
 #include "../classes/util.h"
+#include "json/json.h"
 
 USING_NS_CW;
 
@@ -35,10 +36,43 @@ int32_t main(int32_t argc, char* argv[])
     }
 
 	// 网站文本UTF8==>控制台显示文本gbk
-	char* gbk_str = new char[response.size() + 1];
-	util::utf8_to_gbk(response.c_str(), gbk_str);
-	cout << gbk_str << endl;
-	delete[] gbk_str;
+	char* gbk_data = new char[response.size() + 1];
+	util::utf8_to_gbk(response.c_str(), gbk_data);
+    string gbk_response(gbk_data);
+	cout << gbk_response << endl;
+	delete[] gbk_data;
+
+    // JSON解析测试
+    Json::Reader reader;
+    Json::Value root;
+    if (reader.parse(gbk_response, root, false))
+    {
+        Json::Value json_status = root["status"];
+        cout << "<===================JSON-PARSE-TEST===================>" << endl;
+        cout << "请求返回状态：" << json_status.asString() << endl;
+        Json::Value json_city_array = root["weather"];
+        for (uint32_t index = 0; index <json_city_array.size(); ++index)
+        {
+            Json::Value json_weather = json_city_array[index];
+            
+            cout << "城市名称：" << json_weather["city_name"].asString() << endl;
+            cout << "城市ID：" << json_weather["city_id"].asString() << endl;
+            cout << "更新时间：" << json_weather["last_update"].asString() << endl;
+            Json::Value json_now = json_weather["now"];
+            cout << "天气情况：" << json_now["text"].asString() << endl;
+            cout << "天气ID：" << json_now["code"].asString() << endl;
+            cout << "温度：" << json_now["temperature"].asString() << endl;
+            cout << "体感温度：" << json_now["feels_like"].asString() << endl;
+            cout << "风向：" << json_now["wind_direction"].asString() << endl;
+            cout << "风速：" << json_now["wind_speed"].asString() << endl;
+            cout << "风力等级：" << json_now["wind_scale"].asString() << endl;
+            cout << "湿度：" << json_now["humidity"].asString() << endl;
+            cout << "可见距离：" << json_now["visibility"].asString() << endl;
+            cout << "气压：" << json_now["pressure"].asString() << endl;
+            cout << "气压变化：" << json_now["pressure_rising"].asString() << endl;
+        }
+        cout << "<==========================END========================>" << endl;
+    }
 
 	system("pause");
     return 0;
