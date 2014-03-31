1e737d177a7975635f0557039d5026225c11308a
zhanghao <120910383@qq.com>
2014-03-31 18:31:50 +0800

    对jsoncpp包装，方便扩展适配

---
+++ chinese_weather/classes/json.cpp
@@ -0,0 +1,97 @@
+﻿#include "json.h"
+
+#include "json/json.h"
+using namespace std;
+
+NS_CW_BEGIN;
+
+json::json(value_type_enum value_type)
+{
+	m_impl = new Json::Value;
+}
+
+json::~json()
+{
+	if (NULL != m_impl)
+	{
+		delete m_impl;
+	}
+}
+
+json::value_type_enum json::get_value_type() const
+{
+	return (value_type_enum)(m_impl->type());
+}
+
+std::string json::get_string(std::string default_value) const
+{
+	return m_impl->asString();
+}
+
+bool json::get_bool(bool default_value) const
+{
+	return m_impl->asBool();
+}
+
+int32_t json::get_int32(int32_t default_value) const
+{
+	return m_impl->asInt();
+}
+
+uint32_t json::get_uint32(uint32_t default_value) const
+{
+	return m_impl->asUInt();
+}
+
+int64_t json::get_int64(int64_t default_value) const
+{
+	return m_impl->asInt();
+}
+
+uint64_t json::get_uint64(uint64_t default_value) const
+{
+	return m_impl->asUInt();
+}
+
+float json::get_float(float default_value) const
+{
+	return m_impl->asDouble();
+}
+
+double json::get_double(double default_value) const
+{
+	return m_impl->asDouble();
+}
+
+timestamp_t json::get_timestamp(timestamp_t default_value) const
+{
+	return m_impl->asInt();
+}
+
+void json::get_children(std::vector<json>& children) const
+{
+
+}
+
+bool json::assign_json(const std::string& json_content)
+{
+	Json::Reader reader;
+	return reader.parse(json_content, *m_impl, false);
+}
+
+json json::operator[] (const std::string& key)
+{
+	return json();
+}
+
+bool json::empty() const
+{
+	return m_impl->empty();
+}
+
+void json::clear()
+{
+	m_impl->clear();
+}
+
+NS_CW_END;
---
+++ chinese_weather/classes/json.h
@@ -0,0 +1,58 @@
+﻿#ifndef __CHINESE_WEATHER__JSON_H_
+#define __CHINESE_WEATHER__JSON_H_
+
+#include "define.h"
+#include <string>
+#include <vector>
+namespace Json { class Value; }
+
+NS_CW_BEGIN;
+
+class json
+{
+public:
+	/// 节点类型
+	enum value_type_enum
+	{
+		null_value = 0,
+		int_value,
+		uint_value,
+		real_value,
+		string_value,
+		boolean_value,
+		array_value,
+		object_value,
+	};
+
+public:
+	json(value_type_enum value_type = null_value);
+	~json();
+
+public:
+	value_type_enum get_value_type() const;
+	std::string get_string(std::string default_value = std::string()) const;
+	bool get_bool(bool default_value = false) const;
+	int32_t get_int32(int32_t default_value = 0) const;
+	uint32_t get_uint32(uint32_t default_value = 0) const;
+	int64_t get_int64(int64_t default_value = 0) const;
+	uint64_t get_uint64(uint64_t default_value = 0) const;
+	float get_float(float default_value = 0) const;
+	double get_double(double default_value = 0) const;
+	timestamp_t get_timestamp(timestamp_t default_value = 0) const;
+	void get_children(std::vector<json>& children) const;
+
+public:
+	bool assign_json(const std::string& json_content);
+	json operator[] (const std::string& key);
+
+public:
+	bool empty() const;
+	void clear();
+
+private:
+	Json::Value* m_impl;
+};
+
+NS_CW_END;
+
+#endif // __CHINESE_WEATHER__SINGLETON_H_
